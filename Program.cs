a1239e4cd1d8ca0e5383075dda1926fc7c7b6cba
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-13 19:40:52 +0530

    Vowel and consonant

--- Program.cs
+++ Program.cs
@@ -1,44 +1,16 @@
-﻿using HelloWorld;
+﻿using Logicalpoblems;
 
-namespace BasicProgram
+namespace Logicalpoblems
 {
-    public class Program
+    class Program
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine("Please enter the value for number of flips");
-            //int flip = Convert.ToInt32(Console.ReadLine());
-            //double HeadPER = Flipcoin.CalculatePercentage(flip, "Hello");
-            //Console.WriteLine("Head Percentage " + HeadPER);
-            Primefactorization.CheckPrimefactor();
-            // Harmonic.Harmonicnumber();
-            //Poweroftwo.CalaculateNumber();
-            //Leapyear.LeapyearorNot();
-            //username.Displayusername();
-             //Flipcoin.CalculatePercentage();
-            //Console.WriteLine("Shruthi Welcome to .Net Framwork ");
-
-
-
-            /* int num = 4;
-             double num1 = 1.8;
-             bool val = true;
-             char singleChar = 's';
-             string name = "Shruthi";
-             Console.WriteLine("Integer : " + num);
-             Console.WriteLine("double : " + num1);
-             Console.WriteLine("bool : " + val);
-             Console.WriteLine("singleChar : " + singleChar);
-             Console.WriteLine("String :" + name);
-             int nums = 100;
-             Console.WriteLine(nums);
-             string str = "Shruthi";
-             Console.WriteLine(str + nums);
-             Console.WriteLine("This is {0} number !!!");
-
-             string takeInput = Console.ReadLine();
-             Console.WriteLine("User input :" + takeInput);
-             Console.WriteLine("The name is  {0} {1} {2} {3} ", takeInput, num, num1, val);*/
+           // EvenorOdd.CheckEvenorOdd();
+           // QuotientAndRemainder.FindQuotientAndRemainder();
+            //LargestNumber.FindLargestNumber();
+            // Swapping.SwappingtheNumber();
+            Vowelandconsonant.Checkthecharacter();
         }
     }
 }
